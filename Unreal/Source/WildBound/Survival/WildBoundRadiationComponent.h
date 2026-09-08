d36d204551f502ed54544a11d1917b208129ed84
zackwisdom <107224565+zackwisdom@users.noreply.github.com>
2026-09-08 00:28:54 -0700

    Add player radiation exposure component

---
+++ Unreal/Source/WildBound/Survival/WildBoundRadiationComponent.h
@@ -0,0 +1,49 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "WildBoundRadiationComponent.generated.h"
+
+UCLASS(ClassGroup=(WildBound), meta=(BlueprintSpawnableComponent))
+class WILDBOUND_API UWildBoundRadiationComponent : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:
+	UWildBoundRadiationComponent();
+
+	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
+
+	UPROPERTY(VisibleInstanceOnly, BlueprintReadOnly, Category="WildBound|Radiation")
+	float CurrentExposure = 0.0f;
+
+	UPROPERTY(VisibleInstanceOnly, BlueprintReadOnly, Category="WildBound|Radiation")
+	float AccumulatedDose = 0.0f;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="WildBound|Radiation", meta=(ClampMin="1.0"))
+	float MaxDose = 100.0f;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="WildBound|Radiation", meta=(ClampMin="100.0"))
+	float OuterRadius = 1200.0f;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="WildBound|Radiation", meta=(ClampMin="0.0"))
+	float InnerRadius = 250.0f;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="WildBound|Radiation", meta=(ClampMin="0.0"))
+	float FullExposureDosePerSecond = 1.25f;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="WildBound|Radiation", meta=(ClampMin="0.0", ClampMax="100.0"))
+	float HighDoseDamageThreshold = 70.0f;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="WildBound|Radiation", meta=(ClampMin="0.0"))
+	float HighDoseDamagePerSecond = 1.5f;
+
+	UFUNCTION(BlueprintPure, Category="WildBound|Radiation")
+	float GetExposurePercent() const;
+
+	UFUNCTION(BlueprintPure, Category="WildBound|Radiation")
+	float GetDosePercent() const;
+
+	UFUNCTION(BlueprintPure, Category="WildBound|Radiation")
+	bool IsExposed() const { return CurrentExposure > 1.0f; }
+};
