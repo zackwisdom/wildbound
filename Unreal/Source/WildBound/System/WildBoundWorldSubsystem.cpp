e74d0a8862f8cf9a3b6913e9dcef317760f643d4
zackwisdom <107224565+zackwisdom@users.noreply.github.com>
2026-09-07 22:15:11 -0700

    Fix SOverlay include for UE 5.8

--- Unreal/Source/WildBound/System/WildBoundWorldSubsystem.cpp
+++ Unreal/Source/WildBound/System/WildBoundWorldSubsystem.cpp
@@ -4,7 +4,7 @@
 #include "Engine/GameViewportClient.h"
 #include "GameFramework/Pawn.h"
 #include "GameFramework/PlayerController.h"
-#include "Widgets/Layout/SOverlay.h"
+#include "Widgets/SOverlay.h"
 #include "../Inventory/WildBoundInventoryComponent.h"
 #include "../Player/WildBoundSprintComponent.h"
 #include "../Survival/WildBoundSurvivalComponent.h"
