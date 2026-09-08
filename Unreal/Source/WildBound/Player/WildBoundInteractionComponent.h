6698a21a2a7c3d51a1090e4da475957920353f2d
zackwisdom <107224565+zackwisdom@users.noreply.github.com>
2026-09-08 00:43:01 -0700

    Add scrollable hotbar selection state

--- Unreal/Source/WildBound/Player/WildBoundInteractionComponent.h
+++ Unreal/Source/WildBound/Player/WildBoundInteractionComponent.h
@@ -14,12 +14,17 @@ public:
 
 	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
 
+	UFUNCTION(BlueprintPure, Category="WildBound|Hotbar")
+	int32 GetSelectedHotbarSlot() const { return SelectedHotbarSlot; }
+
 private:
 	float InteractionDistance = 450.0f;
+	int32 SelectedHotbarSlot = 0;
 
 	void TryInteract(AActor* TargetActor);
 	FString GetInteractionPrompt(const AActor* TargetActor) const;
 	void DestroyInteractionGroup(const FName& GroupTag);
-	void HandleQuickUse(class APlayerController& PlayerController);
+	void HandleHotbarSelection(class APlayerController& PlayerController);
+	void TryUseSelectedHotbarItem();
 	void TryUseInventoryItem(FName ItemId);
 };
