e6f654ff02292fcbbb524a062e5b89b22b7e0eb0
zackwisdom <107224565+zackwisdom@users.noreply.github.com>
2026-09-08 11:55:01 -0700

    Centralize world interaction prompt state

--- Unreal/Source/WildBound/Player/WildBoundInteractionComponent.h
+++ Unreal/Source/WildBound/Player/WildBoundInteractionComponent.h
@@ -17,11 +17,18 @@ public:
 	UFUNCTION(BlueprintPure, Category="WildBound|Hotbar")
 	int32 GetSelectedHotbarSlot() const { return SelectedHotbarSlot; }
 
+	void SetContextPrompt(const FString& Prompt, int32 Priority = 0);
+	FString GetContextPrompt() const;
+
 private:
 	float InteractionDistance = 450.0f;
 	int32 SelectedHotbarSlot = 0;
+	FString ContextPrompt;
+	float ContextPromptExpiresAt = -1.0f;
+	int32 ContextPromptPriority = MIN_int32;
 
 	void TryInteract(AActor* TargetActor);
+	void TryPryTarget(AActor* TargetActor);
 	void SearchLootContainer(AActor* TargetActor);
 	FString GetInteractionPrompt(const AActor* TargetActor) const;
 	void DestroyInteractionGroup(const FName& GroupTag);
