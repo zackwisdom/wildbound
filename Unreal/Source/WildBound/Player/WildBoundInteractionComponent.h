b657f8597666863b66a0d7f6ea00fb18d0091b2d
zackwisdom <107224565+zackwisdom@users.noreply.github.com>
2026-09-08 00:17:28 -0700

    Add WildBound interaction component interface

---
+++ Unreal/Source/WildBound/Player/WildBoundInteractionComponent.h
@@ -0,0 +1,23 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "WildBoundInteractionComponent.generated.h"
+
+UCLASS(ClassGroup=(WildBound), meta=(BlueprintSpawnableComponent))
+class WILDBOUND_API UWildBoundInteractionComponent : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:
+	UWildBoundInteractionComponent();
+
+	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
+
+private:
+	float InteractionDistance = 450.0f;
+
+	void TryInteract(AActor* TargetActor);
+	FString GetInteractionPrompt(const AActor* TargetActor) const;
+	void DestroyInteractionGroup(const FName& GroupTag);
+};
