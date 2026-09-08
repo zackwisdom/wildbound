10de2ba71ae93a81b7a9770cda2701ab27710ffa
zackwisdom <107224565+zackwisdom@users.noreply.github.com>
2026-09-08 12:17:11 -0700

    Redesign backpack UI for item management

--- Unreal/Source/WildBound/UI/SWildBoundBackpackWidget.h
+++ Unreal/Source/WildBound/UI/SWildBoundBackpackWidget.h
@@ -4,25 +4,28 @@
 #include "Styling/SlateColor.h"
 #include "Widgets/SCompoundWidget.h"
 
+class UWildBoundBackpackComponent;
 class UWildBoundInventoryComponent;
 
 class SWildBoundBackpackWidget : public SCompoundWidget
 {
 public:
 	SLATE_BEGIN_ARGS(SWildBoundBackpackWidget) {}
-		SLATE_ARGUMENT(TWeakObjectPtr<UWildBoundInventoryComponent>, InventoryComponent)
+		SLATE_ARGUMENT(TWeakObjectPtr<UWildBoundBackpackComponent>, BackpackComponent)
 	SLATE_END_ARGS()
 
 	void Construct(const FArguments& InArgs);
-	void SetInventoryComponent(UWildBoundInventoryComponent* InInventoryComponent);
+	void SetBackpackComponent(UWildBoundBackpackComponent* InBackpackComponent);
 
 private:
-	TWeakObjectPtr<UWildBoundInventoryComponent> InventoryComponent;
+	TWeakObjectPtr<UWildBoundBackpackComponent> BackpackComponent;
 
+	const UWildBoundInventoryComponent* GetInventory() const;
 	TOptional<float> GetWeightPercent() const;
 	FText GetWeightText() const;
 	FText GetStatusText() const;
 	FSlateColor GetStatusColor() const;
-	FText GetSuppliesText() const;
-	FText GetMaterialsText() const;
+	FText GetHotbarText() const;
+	FText GetInventoryListText() const;
+	FText GetSelectedItemText() const;
 };
