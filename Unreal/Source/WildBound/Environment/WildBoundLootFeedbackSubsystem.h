14d3ac1ab2eead2d3e6697e01f4dfe733243154b
zackwisdom <107224565+zackwisdom@users.noreply.github.com>
2026-09-08 12:47:07 -0700

    Add loot feedback subsystem header

---
+++ Unreal/Source/WildBound/Environment/WildBoundLootFeedbackSubsystem.h
@@ -0,0 +1,26 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Subsystems/WorldSubsystem.h"
+#include "TimerManager.h"
+#include "WildBoundLootFeedbackSubsystem.generated.h"
+
+UCLASS()
+class WILDBOUND_API UWildBoundLootFeedbackSubsystem : public UWorldSubsystem
+{
+	GENERATED_BODY()
+
+public:
+	virtual void OnWorldBeginPlay(UWorld& InWorld) override;
+	virtual void Deinitialize() override;
+
+private:
+	FTimerHandle FeedbackTimer;
+	TSet<TWeakObjectPtr<AActor>> ProcessedContainers;
+	TMap<FName, int32> PreviousInventoryCounts;
+	bool bInventorySnapshotInitialized = false;
+
+	void UpdateLootFeedback();
+	void ProcessOpenedContainers();
+	void ProcessInventoryPickupFeedback();
+};
