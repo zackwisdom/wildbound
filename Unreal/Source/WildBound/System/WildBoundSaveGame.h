10adc9a81098738311986095f0b846998f1b150a
zackwisdom <107224565+zackwisdom@users.noreply.github.com>
2026-09-19 23:12:11 -0700

    Add WildBound save game data model

---
+++ Unreal/Source/WildBound/System/WildBoundSaveGame.h
@@ -0,0 +1,78 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/SaveGame.h"
+#include "../Inventory/WildBoundInventoryComponent.h"
+#include "../Player/WildBoundInteractionComponent.h"
+#include "WildBoundEvidenceLogSubsystem.h"
+#include "WildBoundSaveGame.generated.h"
+
+UCLASS()
+class WILDBOUND_API UWildBoundSaveGame : public USaveGame
+{
+	GENERATED_BODY()
+
+public:
+	UPROPERTY(SaveGame)
+	int32 SaveVersion = 1;
+
+	UPROPERTY(SaveGame)
+	FVector PlayerLocation = FVector::ZeroVector;
+
+	UPROPERTY(SaveGame)
+	FRotator PlayerRotation = FRotator::ZeroRotator;
+
+	UPROPERTY(SaveGame)
+	TArray<FWildBoundInventoryStack> InventoryStacks;
+
+	UPROPERTY(SaveGame)
+	TArray<FName> HotbarSlots;
+
+	UPROPERTY(SaveGame)
+	float Health = 100.0f;
+
+	UPROPERTY(SaveGame)
+	float Hunger = 100.0f;
+
+	UPROPERTY(SaveGame)
+	float Thirst = 100.0f;
+
+	UPROPERTY(SaveGame)
+	float Stamina = 100.0f;
+
+	UPROPERTY(SaveGame)
+	float RadiationDose = 0.0f;
+
+	UPROPERTY(SaveGame)
+	float RadiationTreatmentProtectionRemaining = 0.0f;
+
+	UPROPERTY(SaveGame)
+	float BleedingSeverity = 0.0f;
+
+	UPROPERTY(SaveGame)
+	float FractureSeverity = 0.0f;
+
+	UPROPERTY(SaveGame)
+	float PainSeverity = 0.0f;
+
+	UPROPERTY(SaveGame)
+	TArray<FWildBoundEvidenceEntry> EvidenceEntries;
+
+	UPROPERTY(SaveGame)
+	TArray<FWildBoundPersistentContainerState> ContainerStates;
+
+	UPROPERTY(SaveGame)
+	TArray<FVector> InspectedClueLocations;
+
+	UPROPERTY(SaveGame)
+	bool bWaterSupplyCollected = false;
+
+	UPROPERTY(SaveGame)
+	bool bMedicalSupplyCollected = false;
+
+	UPROPERTY(SaveGame)
+	bool bFoodSupplyCollected = false;
+
+	UPROPERTY(SaveGame)
+	bool bCommercialGateOpened = false;
+};
