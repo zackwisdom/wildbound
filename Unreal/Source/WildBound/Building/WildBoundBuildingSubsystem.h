0e95e8cc540b20ea3550d66febaaf1fbbbc8b1d7
zackwisdom <107224565+zackwisdom@users.noreply.github.com>
2026-09-24 16:32:46 -0700

    Add snapping relocation and dismantle state

--- Unreal/Source/WildBound/Building/WildBoundBuildingSubsystem.h
+++ Unreal/Source/WildBound/Building/WildBoundBuildingSubsystem.h
@@ -17,6 +17,9 @@ struct FWildBoundPlacedBuildState
 	GENERATED_BODY()
 
 	UPROPERTY(SaveGame, BlueprintReadOnly, Category="WildBound|Building")
+	int32 BuildId = 0;
+
+	UPROPERTY(SaveGame, BlueprintReadOnly, Category="WildBound|Building")
 	FName BuildTypeId = NAME_None;
 
 	UPROPERTY(SaveGame, BlueprintReadOnly, Category="WildBound|Building")
@@ -51,6 +54,7 @@ private:
 	TWeakObjectPtr<AStaticMeshActor> PreviewActor;
 	TWeakObjectPtr<UMaterialInstanceDynamic> PreviewMaterial;
 	TArray<FWildBoundPlacedBuildState> PlacedBuilds;
+	TMap<TWeakObjectPtr<AActor>, int32> BuildIdByActor;
 	TMap<FName, int32> ActiveMaterialCosts;
 
 	FName ActiveBuildTypeId = NAME_None;
@@ -60,12 +64,22 @@ private:
 	bool bPlacementActive = false;
 	bool bPlacementValid = false;
 	bool bGridSnapEnabled = true;
+	bool bPieceSnapped = false;
+	bool bRelocatingBuild = false;
+	int32 RelocatingBuildId = 0;
+	FWildBoundPlacedBuildState RelocationOriginalState;
+	int32 NextBuildId = 1;
+	int32 PendingDismantleBuildId = 0;
+	float DismantleHoldStartedAt = -1.0f;
 	float PlacementStartedAt = 0.0f;
 	float CurrentYaw = 0.0f;
 
 	void UpdateBuildingMode();
+	void UpdateManagementMode();
 	void UpdatePreviewTransform();
 	void TryPlaceActiveBuild();
+	void BeginRelocation(int32 BuildId);
+	void DismantleBuild(int32 BuildId);
 	void CancelPlacement(bool bShowMessage = true);
 	void EnsurePreviewActor();
 	void DestroyPreviewActor();
@@ -81,7 +95,17 @@ private:
 		const FVector& Location,
 		const FRotator& Rotation,
 		TArray<AActor*>* OutSpawnedActors = nullptr);
+	void RegisterBuildActors(int32 BuildId, const TArray<AActor*>& Actors);
+	void DestroyBuildActors(int32 BuildId);
 	void DestroyAllPlacedBuildActors();
+	int32 FindBuildIdForActor(const AActor* Actor) const;
+	FWildBoundPlacedBuildState* FindBuildState(int32 BuildId);
+	const FWildBoundPlacedBuildState* FindBuildState(int32 BuildId) const;
+	bool CanManageBuild(int32 BuildId, bool bShowMessage) const;
+	TMap<FName, int32> GetMaterialCostsForBuild(FName BuildTypeId) const;
+	FString GetBuildDisplayName(FName BuildTypeId) const;
+	bool TryApplyPieceSnap(FVector& InOutLocation, FRotator& InOutRotation) const;
+	bool IsDuplicatePlacement(const FVector& Location, FName IgnoreBuildType = NAME_None, int32 IgnoreBuildId = 0) const;
 
 	UWildBoundInventoryComponent* GetPlayerInventory() const;
 	FVector GetBuildHalfExtents(FName BuildTypeId) const;
